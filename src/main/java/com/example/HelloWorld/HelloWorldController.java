453f90a3e32940a64291613440a0bff5678043a4
Taylor Samaha <77979107+taylorsam2@users.noreply.github.com>
2021-02-03 19:56:16 -0500

    Update HelloWorldController.java
    Hello msg with my name for homework 1

--- src/main/java/com/example/HelloWorld/HelloWorldController.java
+++ src/main/java/com/example/HelloWorld/HelloWorldController.java
@@ -9,6 +9,6 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String index()
     {
-        return "Hello World from ABC inc.!";
+        return "Hello Taylor Samaha!";
     }
 }
